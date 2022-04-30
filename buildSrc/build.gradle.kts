defd281e1ba98bf550676325da69c8c0f551d32b
wetabq <wetabq@gmail.com>
2022-04-30 19:56:19 +0800

    fix(all): remove redundant `sourceSet` generated by gradle
    remove `groovy`, `java` and `proto` folders automatically generated by gradle after project refresh

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,3 +9,22 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 }
+
+sourceSets {
+    main {
+        groovy {
+            setSrcDirs(emptySet<File>()) // No Groovy
+        }
+        java {
+            setSrcDirs(setOf("kotlin")) // No Java
+        }
+    }
+    test {
+        groovy {
+            setSrcDirs(emptySet<File>())
+        }
+        java {
+            setSrcDirs(setOf("kotlin"))
+        }
+    }
+}
