cbb85a4ea8374f3e1f20bbacbcb63516d0198a25
wetabq <wetabq@gmail.com>
2022-05-03 02:43:39 +0800

    build(core): add dependencies related with submodules

--- sorapointa-core/build.gradle.kts
+++ sorapointa-core/build.gradle.kts
@@ -6,5 +6,11 @@ plugins {
 version = "0.1.0-Dev"
 
 dependencies {
-
+    implementation(project(":sorapointa-dataprovider"))
+    implementation(project(":sorapointa-dispatch"))
+    implementation(project(":sorapointa-event"))
+    implementation(project(":sorapointa-i18n"))
+    implementation(project(":sorapointa-kcp"))
+    implementation(project(":sorapointa-proto"))
+    implementation(project(":sorapointa-utils"))
 }
