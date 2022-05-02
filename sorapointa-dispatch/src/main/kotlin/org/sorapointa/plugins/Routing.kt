00c10fef0ff7d95e2f545c923bd5dd5d24bf9d02
wetabq <wetabq@gmail.com>
2022-05-03 02:38:23 +0800

    feature(dispatch): partially implement dispatch (still working)

--- sorapointa-dispatch/src/main/kotlin/org/sorapointa/plugins/Routing.kt
+++ sorapointa-dispatch/src/main/kotlin/org/sorapointa/plugins/Routing.kt
@@ -1,14 +1,23 @@
 package org.sorapointa.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Hello Sorapointa! The Dispatch Server has been successfully launched!")
         }
+        get("/query_region_list") {
+            logger.info { "Client ${call.request.local.host} has queried region list" }
+            
+        }
+
     }
 }
