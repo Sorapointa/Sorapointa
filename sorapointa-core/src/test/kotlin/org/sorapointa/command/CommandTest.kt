0774187bb724aaa296df1b60dc573b2d62e0c185
企鹅2035 <qie2035@qq.com>
2022-05-08 14:17:58 +0800

    fix(command): Fix some bugs and add some docs & notes. (#15)
    * build(core): fix the project "sorapointa-event" not found
    
    * fix(command): fix help page number exceed
    
    * docs(command): A simple bad README.md
    
    * docs(command): Add some docs and notes.

--- sorapointa-core/src/test/kotlin/org/sorapointa/command/CommandTest.kt
+++ sorapointa-core/src/test/kotlin/org/sorapointa/command/CommandTest.kt
@@ -13,6 +13,7 @@ import java.util.*
 class CommandTest {
     @Test
     fun commandTest() {
+        // Register en.
         this::class.java.getResource(
             "/i18n/${Locale.ENGLISH.toLanguageTag()}.json"
         )?.also {
@@ -20,6 +21,7 @@ class CommandTest {
                 I18nManager.registerLanguage(File(it.toURI()))
             }
         }
+        // Register zh-CN.
         this::class.java.getResource(
             "/i18n/${Locale.CHINA.toLanguageTag()}.json"
         )?.also {
@@ -28,24 +30,41 @@ class CommandTest {
                 I18nConfig.reload()
             }
         }
+        // Register commands.
         CommandManager.registerCommand(TestCommand)
         CommandManager.registerCommand(HelpCommand)
+        // Test commands
         println("Test command: help")
         CommandManager.invokeCommand(TestSender, "help")
         println("Test command: help 2")
         CommandManager.invokeCommand(TestSender, "help 2")
+        println("Test command: help 5")
+        CommandManager.invokeCommand(TestSender, "help 5")
         println("Test command: help --help")
         CommandManager.invokeCommand(TestSender, "help --help")
+        println("Test command with type=PLAYER: help")
+        CommandManager.invokeCommand(PlayerSender, "help")
         println("Test finish.")
     }
 }
 
+/** A sender for test. */
 object TestSender : CommandSender() {
     override fun sendMessage(msg: String) {
         println(msg)
     }
 }
 
+/** A sender with type=PLAYER for test. */
+object PlayerSender : CommandSender(
+    CommandSenderType.PLAYER
+) {
+    override fun sendMessage(msg: String) {
+        println(msg)
+    }
+}
+
+/** A command for test. */
 object TestCommand : SorapointaCommand(
     name = "test",
     type = CommandSenderType.ADMIN,
