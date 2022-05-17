ca86a45208648e889211389dbbd9844e84ac5e60
wetabq <wetabq@gmail.com>
2022-05-17 13:14:08 +0800

    feature(event): implement a graceful state controller

---
+++ sorapointa-event/src/main/kotlin/org/sorapointa/event/StateController.kt
@@ -0,0 +1,134 @@
+package org.sorapointa.event
+
+import org.sorapointa.utils.ModuleScope
+
+import kotlinx.atomicfu.atomic
+import kotlinx.atomicfu.update
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import java.util.concurrent.ConcurrentHashMap
+
+
+interface WithState<out T: Enum<*>> {
+
+    val state: T
+
+    suspend fun startState() {
+
+    }
+
+    suspend fun endState() {
+
+    }
+
+}
+
+class StateController<TState : Enum<*>, TInterfaceWithState: WithState<TState>, TClassWithState: TInterfaceWithState> (
+    private var scope: ModuleScope,
+    private var parentStateClass: TClassWithState,
+    vararg stateInstances: TInterfaceWithState
+) {
+
+    private val states = listOf(*stateInstances)
+
+    private var currentState = atomic(states.first())
+
+    private var observers = ConcurrentHashMap<suspend TClassWithState.(TState, TState) -> Unit, ListenerState>()
+    private var interceptors = ConcurrentHashMap<suspend TClassWithState.(TState, TState) -> Boolean, ListenerState>()
+
+
+    suspend fun setState(afterState: TState): TInterfaceWithState {
+        val before = currentState.value
+        val beforeState = before.state
+        val after = states.first { it.state == afterState }
+        if (invokeChange(beforeState, afterState, ListenerState.BEFORE_UPDATE, parentStateClass)) return before
+        before.endState()
+        currentState.update { after }
+        after.startState()
+        invokeChange(beforeState, afterState, ListenerState.AFTER_UPDATE, parentStateClass)
+        return before
+    }
+
+    private suspend fun invokeChange(
+        beforeState: TState,
+        afterState: TState,
+        listenerState: ListenerState,
+        listenerCaller: TClassWithState
+    ): Boolean {
+        var isIntercepted by atomic(false)
+        observers
+            .asSequence()
+            .filter { it.value == listenerState }
+            .forEach {(observer, _) ->
+                scope.launch {
+                    listenerCaller.observer(beforeState, afterState)
+                }
+            }
+        interceptors
+            .asSequence()
+            .asFlow()
+            .filter { it.value == listenerState }
+            .map {(interceptor, _) ->
+                scope.launch {
+                    isIntercepted = listenerCaller.interceptor(beforeState, afterState) || isIntercepted
+                }
+            }
+            .collect { it.join() }
+        return isIntercepted
+    }
+
+    fun getState(): TInterfaceWithState {
+        return currentState.value
+    }
+
+    fun observeStateChange(
+        listenerState: ListenerState = ListenerState.BEFORE_UPDATE,
+        observer: suspend TClassWithState.(TState, TState) -> Unit
+    ) {
+        observers[observer] = listenerState
+    }
+
+    fun interceptStateChange(
+        listenerState: ListenerState = ListenerState.BEFORE_UPDATE,
+        interceptor: suspend TClassWithState.(TState, TState) -> Boolean
+    ) {
+        interceptors[interceptor] = listenerState
+    }
+
+    fun cleanAllObserver() {
+        observers.clear()
+    }
+
+    fun cleanAllInterceptor() {
+        interceptors.clear()
+    }
+
+    enum class ListenerState {
+        BEFORE_UPDATE,
+        AFTER_UPDATE
+    }
+
+
+}
+
+
+inline fun <TState : Enum<*>, TInterfaceWithState: WithState<TState>, TClassWithState: TInterfaceWithState>
+    StateController<TState, TInterfaceWithState, TClassWithState>.observe(
+    listenerState: StateController.ListenerState = StateController.ListenerState.BEFORE_UPDATE,
+    crossinline observer: suspend TClassWithState.() -> Unit
+) = observeStateChange(listenerState) { _, _ -> this.observer() }
+
+inline fun <TState : Enum<*>, TInterfaceWithState: WithState<TState>, TClassWithState: TInterfaceWithState>
+    StateController<TState, TInterfaceWithState, TClassWithState>.intercept(
+    listenerState: StateController.ListenerState = StateController.ListenerState.BEFORE_UPDATE,
+    crossinline interceptor: suspend TClassWithState.() -> Boolean
+) = interceptStateChange(listenerState) { _, _ -> this.interceptor() }
+
+inline fun <TState : Enum<*>, TInterfaceWithState: WithState<TState>, TClassWithState: TInterfaceWithState>
+    StateController<TState, TInterfaceWithState, TClassWithState>.block(
+    listenerState: StateController.ListenerState = StateController.ListenerState.BEFORE_UPDATE,
+    crossinline interceptor: suspend TClassWithState.() -> Unit
+) = interceptStateChange(listenerState) { _, _ -> this.interceptor(); false }
+
