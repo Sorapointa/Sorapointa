43fd2ef28ba289bdacaad5e9709565e9f82bf84e
WetABQ <wetabq@gmail.com>
2022-05-29 20:36:41 +0800

    feature(dispatch): ec2b dynamical generation, clientConfig can be config, turn off passwordPepper option by default

--- sorapointa-dispatch/src/main/kotlin/org/sorapointa/dispatch/DispatchServer.kt
+++ sorapointa-dispatch/src/main/kotlin/org/sorapointa/dispatch/DispatchServer.kt
@@ -10,11 +10,14 @@ import io.ktor.server.netty.*
 import io.ktor.util.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import org.slf4j.LoggerFactory
 import org.sorapointa.data.provider.DataFilePersist
 import org.sorapointa.data.provider.DatabaseConfig
 import org.sorapointa.data.provider.DatabaseManager
 import org.sorapointa.dispatch.data.AccountTable
+import org.sorapointa.dispatch.data.ClientCustomConfig
+import org.sorapointa.dispatch.data.RegionListClientCustomConfig
 import org.sorapointa.dispatch.plugins.*
 import org.sorapointa.dispatch.plugins.configureHTTP
 import org.sorapointa.dispatch.plugins.configureMonitoring
@@ -22,9 +25,11 @@ import org.sorapointa.dispatch.plugins.configureRouting
 import org.sorapointa.dispatch.plugins.configureSerialization
 import org.sorapointa.dispatch.utils.KeyProvider
 import org.sorapointa.utils.*
+import org.sorapointa.utils.crypto.Ec2bData
 import org.sorapointa.utils.randomByteArray
 import org.sorapointa.utils.encoding.hex
 import java.io.File
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.text.toCharArray
 import kotlin.time.Duration
 
@@ -34,6 +39,8 @@ internal fun main(): Unit = runBlocking {
 }
 
 object DispatchServer {
+
+    var dispatchKeyMap = ConcurrentHashMap<String, Ec2bData>()
 
     internal val client by lazy {
         HttpClient(CIO) {
@@ -101,36 +108,67 @@ object DispatchConfig : DataFilePersist<DispatchConfig.Data>(
     File(configDirectory, "dispatchConfig.json"), Data()
 ) {
 
-    @kotlinx.serialization.Serializable
+    @Serializable
     data class Data(
         val host: String = "0.0.0.0",
         val port: Int = 443,
-        // TODO: Extract to multiple setting
         val gateServerIp: String = "localhost",
         val gateServerPort: Int = 22101,
         val servers: ArrayList<Server> = arrayListOf(Server()),
+        val useSSL: Boolean = true,
+        var certification: Certification = Certification(),
+        val requestSetting: RequestSetting = RequestSetting(),
+        val accountSetting: AccountSetting = AccountSetting(),
+        val regionListClientCustomConfig: RegionListClientCustomConfig =  RegionListClientCustomConfig(
+            sdkEnvironment = 2u,
+            showException = false,
+            loadPatch = false,
+            regionConfig = "",
+            regionDispatchType = 0u,
+            videoKey = 5578228838233776,
+            downloadMode = 0u
+        ),
+        val clientCustomConfig: ClientCustomConfig = ClientCustomConfig(
+            codeSwitch = listOf(15u, 2410u, 2324u, 21u),
+            coverSwitch = listOf(8u, 40u),
+            perfReportEnable = false,
+            homeDotPattern = true,
+            homeItemFilter = 20u,
+            reportNetDelayConfig = ClientCustomConfig.ReportNetDelayConfigData(
+                openGateServer = true
+            )
+        )
+    )
+
+
+    @Serializable
+    data class RequestSetting(
         val forwardCommonRequest: Boolean = true,
         // If false, dispatch server will use default config hardcoded in Sorapointa
         val forwardQueryCurrRegion: Boolean = true,
         val queryCurrRegionHardcode: String = QUERY_CURR_HARDCODE_DEFAULT,
+        @SerialName("v2.8responseFormat")
+        val v28: Boolean = false
+    )
+
+    @Serializable
+    data class AccountSetting(
         @SerialName("comboExpiredExpiredTime")
         private val _comboTokenExpiredTime: String = "3d",
         @SerialName("dispatchTokenExpiredTime")
         private val _dispatchTokenExpiredTime: String = "3d",
-        @SerialName("v2.8responseFormat") val v28: Boolean = false,
-        val password: Argon2PasswordSetting = Argon2PasswordSetting(),
-        val useSSL: Boolean = true,
-        var certification: Certification = Certification()
+        val password: Argon2PasswordSetting = Argon2PasswordSetting()
     ) {
         val comboTokenExpiredTime: Duration
-            get() = Duration.parse(_comboTokenExpiredTime)
+        get() = Duration.parse(_comboTokenExpiredTime)
 
         val dispatchTokenExpiredTime: Duration
-            get() = Duration.parse(_dispatchTokenExpiredTime)
+        get() = Duration.parse(_dispatchTokenExpiredTime)
     }
 
-    @kotlinx.serialization.Serializable
+    @Serializable
     data class Argon2PasswordSetting(
+        val usePepper: Boolean = false,
         val hashPepper: String = randomByteArray(256).hex,
         val saltByteLength: Int = 64,
         val memory: Int = 12,
@@ -141,7 +179,7 @@ object DispatchConfig : DataFilePersist<DispatchConfig.Data>(
         val argon2Version: Int = 19
     )
 
-    @kotlinx.serialization.Serializable
+    @Serializable
     data class Certification(
         val keyStoreFilePath: String = File(configDirectory, KeyProvider.DEFAULT_CERT_NAME).absPath,
         val keyStore: String = "JKS",
@@ -150,11 +188,11 @@ object DispatchConfig : DataFilePersist<DispatchConfig.Data>(
         val privateKeyPassword: String = KeyProvider.DEFAULT_CERT_PASSWORD
     )
 
-    @kotlinx.serialization.Serializable
+    @Serializable
     data class Server(
         val serverName: String = "sorapointa_01",
         val title: String = "Sorapointa",
         val serverType: String = "DEV_PUBLIC",
-        val dispatchDomain: String = "localhost",
+        val dispatchDomain: String = "localhost"
     )
 }
