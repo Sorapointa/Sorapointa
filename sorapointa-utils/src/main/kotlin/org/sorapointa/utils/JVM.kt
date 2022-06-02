0c81a455ee39b473d744332a24ae156f3dd72cb5
Colerar <233hbj@gmail.com>
2022-06-02 14:26:26 +0800

    feat: util for resources

--- sorapointa-utils/src/main/kotlin/org/sorapointa/utils/JVM.kt
+++ sorapointa-utils/src/main/kotlin/org/sorapointa/utils/JVM.kt
@@ -3,6 +3,12 @@ package org.sorapointa.utils
 import kotlinx.coroutines.runBlocking
 
 /**
+ * get current context class loader
+ */
+val contextClassLoader: ClassLoader?
+    get() = Thread.currentThread().contextClassLoader
+
+/**
  * JVM shutdown hook
  * @param block lambda will be executed after program exited
  */
