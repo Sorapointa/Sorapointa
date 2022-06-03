50c716d5339219459b355c1509c1f0c1c4a80596
Colerar <233hbj@gmail.com>
2022-06-03 16:39:45 +0800

    fix: database init before load
    fix by sorting setup order

--- sorapointa-core/src/main/kotlin/org/sorapointa/config/Configs.kt
+++ sorapointa-core/src/main/kotlin/org/sorapointa/config/Configs.kt
@@ -3,6 +3,7 @@ package org.sorapointa.config
 import org.jetbrains.exposed.sql.Table
 import org.sorapointa.SorapointaConfig
 import org.sorapointa.data.provider.DataFilePersist
+import org.sorapointa.data.provider.DatabaseConfig
 import org.sorapointa.dispatch.DispatchConfig
 import org.sorapointa.dispatch.data.AccountTable
 import org.sorapointa.event.EventManagerConfig
@@ -10,6 +11,7 @@ import org.sorapointa.task.CronTasks
 import org.sorapointa.utils.I18nConfig
 
 internal val registeredConfig: List<DataFilePersist<*>> = listOf(
+    DatabaseConfig,
     SorapointaConfig,
     I18nConfig,
     EventManagerConfig,
