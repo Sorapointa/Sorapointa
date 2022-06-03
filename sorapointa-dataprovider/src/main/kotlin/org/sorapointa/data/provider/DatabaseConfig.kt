456f7ad3d652218dcb6fb6b6c32515cfc563be8f
Colerar <233hbj@gmail.com>
2022-06-03 21:08:31 +0800

    docs: add comment for `DatabaseConfig`

--- sorapointa-dataprovider/src/main/kotlin/org/sorapointa/data/provider/DatabaseConfig.kt
+++ sorapointa-dataprovider/src/main/kotlin/org/sorapointa/data/provider/DatabaseConfig.kt
@@ -2,6 +2,7 @@ package org.sorapointa.data.provider
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import net.mamoe.yamlkt.Comment
 import net.mamoe.yamlkt.Yaml
 import org.sorapointa.config.DbMeta
 import org.sorapointa.utils.absPath
@@ -22,7 +23,7 @@ enum class DatabaseType {
     },
     POSTGRESQL {
         override val defaultConnectionString: String = "jdbc:postgresql://localhost:5432/sorapointa"
-        override val defaultMaxPoolSize: Int = 10
+        override val defaultMaxPoolSize: Int = Runtime.getRuntime().availableProcessors()
         override val driverPath: String = "org.postgresql.Driver"
         override val defaultIsolationLevel: String = "REPEATABLE_READ"
     };
@@ -38,11 +39,45 @@ object DatabaseConfig : DataFilePersist<DatabaseConfig.Data>(
 ) {
     @Serializable
     data class Data(
+        @Comment("Which database you want to use? Available option: SQLITE, POSTGRESQL")
         val type: DatabaseType = DbMeta.DEFAULT_DATABASE_TYPE,
+        @Comment(
+            """
+            JDBC connection string
+            For SQLite, you can change the file location
+            Like: jdbc:sqlite:/abs/path/to/sqlite.db
+            For PostgresQL, you can choose which server to connect
+            Like: jdbc:postgresql://localhost:5432/sorapointa
+        """
+        )
         val connectionString: String = type.defaultConnectionString,
+        @Comment("username, can be empty for SQLite")
         val user: String = "",
+        @Comment("username, can be empty")
         val password: String = "",
+        @Comment(
+            """
+            Max pool size
+            Default and Recommended Value:
+              SQLite: 1, because SQLite has no multi-connection support
+              PostgresQL: Processor Threads, like 12 for 6-core machine
+                see more: https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
+        """
+        )
         val maxPoolSize: Int = type.defaultMaxPoolSize,
+        @Comment(
+            """
+            Available value for isolation level
+            
+            If you don't what it means, just keep default.
+            
+            Lenient / Unsafe / Fast -> Strict / Safe / Slow 
+            SQLite: READ_UNCOMMITTED, SERIALIZABLE
+              see more: https://www.sqlite.org/isolation.html
+            PostgresQL: READ_UNCOMMITTED, READ_COMMITTED, REPEATABLE_READ, SERIALIZABLE
+              see more: https://www.postgresql.org/docs/current/transaction-iso.html
+        """
+        )
         @SerialName("isolationLevel")
         internal val _isolationLevel: String = type.defaultIsolationLevel,
     ) {
