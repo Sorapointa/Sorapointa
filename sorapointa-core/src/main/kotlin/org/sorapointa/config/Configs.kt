b287f1749e3bc3116ec7d6c5d1bca3ed1713f8d1
WetABQ <wetabq@gmail.com>
2022-06-09 02:16:59 +0800

    feat(core): add player and inventory data
    add SQL data structure: `SQLJson`, `SQLMap`, `SQLSet`
    fix SQLite primary key the connection url issue

--- sorapointa-core/src/main/kotlin/org/sorapointa/config/Configs.kt
+++ sorapointa-core/src/main/kotlin/org/sorapointa/config/Configs.kt
@@ -7,6 +7,9 @@ import org.sorapointa.data.provider.DatabaseConfig
 import org.sorapointa.dispatch.DispatchConfig
 import org.sorapointa.dispatch.data.AccountTable
 import org.sorapointa.event.EventManagerConfig
+import org.sorapointa.game.data.InventoryTable
+import org.sorapointa.game.data.ItemTable
+import org.sorapointa.game.data.PlayerDataTable
 import org.sorapointa.task.CronTasks
 import org.sorapointa.utils.I18nConfig
 
@@ -20,5 +23,8 @@ internal val registeredConfig: List<DataFilePersist<*>> = listOf(
 
 internal val registeredDatabaseTable: List<Table> = listOf(
     CronTasks,
-    AccountTable
+    AccountTable,
+    PlayerDataTable,
+    InventoryTable,
+    ItemTable
 )
---
+++ sorapointa-core/src/main/kotlin/org/sorapointa/game/data/InventoryData.kt
@@ -0,0 +1,150 @@
+package org.sorapointa.game.data
+
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IdTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.sorapointa.data.provider.sql.SetTable
+import org.sorapointa.data.provider.sql.jsonb
+import org.sorapointa.proto.MaterialDeleteInfoKt
+import org.sorapointa.proto.MaterialDeleteInfoKt.countDownDelete
+import org.sorapointa.proto.MaterialDeleteInfoKt.dateTimeDelete
+import org.sorapointa.proto.MaterialDeleteInfoKt.delayWeekCountDownDelete
+import org.sorapointa.proto.MaterialDeleteInfoOuterClass
+import org.sorapointa.proto.materialDeleteInfo
+
+internal object InventoryTable : SetTable<UInt, EntityID<ULong>>("inventory") {
+    override val id: Column<EntityID<UInt>> =
+        reference("player", PlayerDataTable, onDelete = ReferenceOption.CASCADE)
+    override val value: Column<EntityID<ULong>> =
+        reference("item_unique_id", ItemTable, onDelete = ReferenceOption.CASCADE)
+
+    override val primaryKey: PrimaryKey = PrimaryKey(id, value)
+}
+
+internal object ItemTable : IdTable<ULong>("inventory_item") {
+    override val id: Column<EntityID<ULong>> = ulong("item_unique_id").autoIncrement().entityId()
+
+    val itemData: Column<ItemData> = jsonb("item_data")
+}
+
+class InventoryItem(id: EntityID<ULong>) : Entity<ULong>(id) {
+    companion object : EntityClass<ULong, InventoryItem>(ItemTable)
+
+    val item by ItemTable.itemData
+}
+
+@Serializable
+sealed class ItemData {
+
+    abstract val itemId: UInt
+    abstract val guid: ULong
+
+    @Serializable
+    data class Material(
+        override val itemId: UInt,
+        override val guid: ULong,
+        val count: UInt,
+        val deleteInfo: MaterialDeleteInfo
+    ) : ItemData()
+
+    @Serializable
+    data class Furniture(
+        override val itemId: UInt,
+        override val guid: ULong,
+        val count: UInt
+    ) : ItemData()
+
+    @Serializable
+    sealed class Equip : ItemData() {
+        abstract val isLocked: Boolean
+
+        @Serializable
+        data class Weapon(
+            override val itemId: UInt,
+            override val guid: ULong,
+            override val isLocked: Boolean,
+            val level: UInt,
+            val exp: UInt,
+            val promoteLevel: UInt,
+            val affixMap: Map<UInt, UInt>
+        ) : Equip()
+
+        @Serializable
+        data class Reliquary(
+            override val itemId: UInt,
+            override val guid: ULong,
+            override val isLocked: Boolean,
+            val level: UInt,
+            val exp: UInt,
+            val promoteLevel: UInt,
+            val mainPropId: UInt,
+            val appendPropIdList: List<UInt>
+        ) : Equip()
+    }
+}
+
+@Serializable
+sealed class MaterialDeleteInfo {
+
+    abstract val hasDeleteConfig: Boolean
+
+    fun toProto(): MaterialDeleteInfoOuterClass.MaterialDeleteInfo =
+        materialDeleteInfo {
+            hasDeleteConfig = this@MaterialDeleteInfo.hasDeleteConfig
+            toProto()
+        }
+
+    abstract fun MaterialDeleteInfoKt.Dsl.toProto()
+
+    @Serializable
+    data class CountDownDelete(
+        override val hasDeleteConfig: Boolean,
+        val deleteTimeNumMap: Map<UInt, UInt>,
+        val configCountDownTime: UInt
+    ) : MaterialDeleteInfo() {
+
+        override fun MaterialDeleteInfoKt.Dsl.toProto() {
+            val t = this@CountDownDelete
+            countDownDelete = countDownDelete {
+                deleteTimeNumMap.putAll(t.deleteTimeNumMap.map { it.key.toInt() to it.value.toInt() }.toMap())
+                configCountDownTime = t.configCountDownTime.toInt()
+            }
+        }
+    }
+
+    @Serializable
+    data class DateTimeDelete(
+        override val hasDeleteConfig: Boolean,
+        val deleteTime: UInt
+    ) : MaterialDeleteInfo() {
+
+        override fun MaterialDeleteInfoKt.Dsl.toProto() {
+            val t = this@DateTimeDelete
+            dateDelete = dateTimeDelete {
+                deleteTime = t.deleteTime.toInt()
+            }
+        }
+    }
+
+    @Serializable
+    data class DelayWeekCountDownDelete(
+        override val hasDeleteConfig: Boolean,
+        val deleteTimeNumMap: Map<UInt, UInt>,
+        val configCountDownTime: UInt,
+        val configDelayWeek: UInt
+    ) : MaterialDeleteInfo() {
+
+        override fun MaterialDeleteInfoKt.Dsl.toProto() {
+            val t = this@DelayWeekCountDownDelete
+            delayWeekCountDownDelete = delayWeekCountDownDelete {
+                deleteTimeNumMap.putAll(t.deleteTimeNumMap.map { it.key.toInt() to it.value.toInt() }.toMap())
+                configCountDownTime = t.configCountDownTime.toInt()
+                configDelayWeek = t.configDelayWeek.toInt()
+            }
+        }
+    }
+}
