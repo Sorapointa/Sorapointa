80a4b0b89f0112d34f2d31d1b15c694d3a3ad555
Colerar <233hbj@gmail.com>
2022-06-10 14:33:04 +0800

    chore: update dependency
    ↑ Kotlin    -> 1.7.0
    ↑ kcp-netty -> 1.6.3
    ↑ yac       -> 1.0.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
     implementation("com.github.gmazzo:gradle-buildconfig-plugin:3.0.3")
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
 }
