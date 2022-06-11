75fb388613f2e2ee0dfa97dc47aa1ffb4684dfc6
Colerar <233hbj@gmail.com>
2022-06-11 21:47:43 +0800

    build: configure `ktlint-gradle` plugin, add git PreCommitHook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,34 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 plugins {
     kotlin("plugin.serialization") apply false
     kotlin("jvm") apply false
+    id("org.jlleitschuh.gradle.ktlint")
     id("org.jetbrains.kotlinx.binary-compatibility-validator")
 }
 
 subprojects {
-    repositories {
-        mavenCentral()
-    }
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
     apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
     afterEvaluate {
         configureLogbackCopy()
     }
+    configure<KtlintExtension> {
+        disabledRules.set(setOf("no-wildcard-imports", "import-ordering"))
+        filter {
+            // exclude("**/generated/**")
+            fun exclude(path: String) = exclude {
+                projectDir.toURI().relativize(it.file.toURI()).normalize().path.contains(path)
+            }
+            setOf("/generated/", "/build/", "resources").forEach { exclude(it) }
+        }
+    }
 }
 
 allprojects {
+    repositories {
+        mavenCentral()
+    }
     group = "moe.sdl.sorapointa"
     version = "0.1.0-SNAPSHOT"
 }
