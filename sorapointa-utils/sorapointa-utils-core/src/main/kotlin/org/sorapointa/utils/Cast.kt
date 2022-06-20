02e1e4f634da888309d302807b85f62ff753ed44
WetABQ <wetabq@gmail.com>
2022-06-21 01:00:20 +0800

    refactor(utils-core): remove unused cast methods

--- sorapointa-utils/sorapointa-utils-core/src/main/kotlin/org/sorapointa/utils/Cast.kt
+++ sorapointa-utils/sorapointa-utils-core/src/main/kotlin/org/sorapointa/utils/Cast.kt
@@ -1,28 +1,22 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "NOTHING_TO_INLINE")
 
 package org.sorapointa.utils
 
 import kotlin.contracts.contract
-
-inline fun <reified T> Any?.cast(): T {
-    contract { returns() implies (this@cast is T) }
-    return this as T
-}
-
-inline fun <reified U : T, T> T.castUp(): U {
-    contract { returns() implies (this@castUp is U) }
-    return this as U
-}
 
 inline fun <reified T> Any?.safeCast(): T? {
     contract { returnsNotNull() implies (this@safeCast is T) }
     return this as? T
 }
 
-inline fun <reified T> Any?.castOrNull(): T? {
-    contract { returnsNotNull() implies (this@castOrNull is T) }
-    return this as? T
-}
-
-@Suppress("NOTHING_TO_INLINE", "UNCHECKED_CAST")
+@Suppress("UNCHECKED_CAST")
 inline fun <T> Any?.uncheckedCast(): T = this as T
+
+inline fun Boolean.toInt(): Int =
+    if (this) 1 else 0
+
+inline fun Boolean.toUInt(): UInt =
+    if (this) 1u else 0u
+
+inline fun Boolean.toUShort(): UShort =
+    if (this) 1u else 0u
