0093caf8e0e298edfe052c7293c4881d9535ad18
WetABQ <wetabq@gmail.com>
2022-06-21 01:15:08 +0800

    feat(dispatch): add RSA signature verify, remove redundant `suspend` modifier, and refactor `UInt` to `Int` in `DispatchData` and also for database

--- sorapointa-dispatch/src/main/kotlin/org/sorapointa/dispatch/DispatchServer.kt
+++ sorapointa-dispatch/src/main/kotlin/org/sorapointa/dispatch/DispatchServer.kt
@@ -113,6 +113,8 @@ private val QUERY_CURR_HARDCODE_DEFAULT =
     "aHR0cHM6Ly9jbmdmZGlzcGF0Y2gueXVhbnNoZW4uY29tL3F1ZXJ5X2N1cl9yZWdpb24/dmVyc2lvbj1DTlJFTFdpbjIuNy4wJmxhbmc9MiZwbGF0Zm9ybT0zJmJpbmFyeT0xJnRpbWU9ODE4JmNoYW5uZWxfaWQ9MSZzdWJfY2hhbm5lbF9pZD0xJmFjY291bnRfdHlwZT0xJmRpc3BhdGNoU2VlZD02N2QzZWUzOWZkYWFiMDlm".decodeBase64String()
 private val QUERY_CURR_RSA_KEY =
     "PFJTQUtleVZhbHVlPjxNb2R1bHVzPnovZnlmb3psRElEV0c5ZTNMYjI5KzdqM2M2Nnd2VUpCYUJXUDEwckI5SFRFNnByamZjR01xQzlpbXI2ekFkRDlxK0dyMWo3ZWd2cWdpM0RhK1ZCQU1GSDkyLzV3RDVQc0Q3ZFg4WjJmNG82NVZrMm5WT1k4RGw3NVovdVJoZzBFdXduZnJ2ZWQ2OXo5TEc2dXRtbHl2NllVUEFmbFhoL0pGdzdEcTZjNEVHZVIrS2VqRlR3bVZoRWR6UEdIalhoRm1zVnQ5SGRYUllTZjROeEhQek93ajh0aVNhT1FBMGpDNEU0bU03cnZHU0g1R1g2aG1hKzdwSm5sLzUrckVWTTBtU1F2bTBtMVhlZm11RnkwNDBiRVovNk83WmVuT0dCc3Z2d3VHM1RUNEZORE56VzhEdzlFeEgxbDZOb1JHYVZrRGR0cmwvbkZ1NSthMDlQbS9FMEVsdz09PC9Nb2R1bHVzPjxFeHBvbmVudD5BUUFCPC9FeHBvbmVudD48UD45aGRVUnhlNkRuT3FTcGU2bmgyblZMVG14clBYTlkrRlNGQ2I0S3R1R0I1T3FtT2VBa2tRSHYyb3lzYWJLU0xRLzl3YTF0TnlzZC96Nkx1QU9VZ1piUTR4dmorT2ZoL2tBSlVQVFNMSytRZElZK2ZRQ0tZeWcwNHh1UWFpM3RLUktlZHpERmQxckRBUEpPN1oyaDllNEd2dmI0WmlxQkVBYm5ZaTREUUxTbEU9PC9QPjxRPjJGZW45VEpiK0cwSGJ0K3NweUgrdE1wQXFiWGFRRlhiUUNTUkZSQlNKdUtKREphNTVZcXo3bHRWcGJsSG1nTWlGYkdwKzBtMmNRVlpTOVpwTWVrZXdIOXVtTkxjSW5wYVNlbzF1bHJkQWhKeWxYVzdEeFg0UzNQOHJiOSsyUEpuTVdpZXB6NG01M25mcmpFVjBpVTZ4R1AyQm1jcnpkWnk2TG9RWEVCNnZtYz08L1E+PERQPm5OUFBOS010UWVwNk9xRXBIM3ljVjRJVms4bW1PNDdrREdxNmU5b2tCaURDVnhtMjU1UHlQeDIrQk1PK3U5OWhPN3prS2NXRTBWQjhXdk9xeWxabFJiZUhBY3YxSGZGcTF1Z25ZU3ZzRi9tSks0bmViTFNsZWtKSnM3VkQ5Q1pTdGxhMlhjWWF5b215RFFKZU9RQkc4VlEzdVdYMTEwOUdiQjdES1FoaHJaRT08L0RQPjxEUT5jbUt1V0ZOZkUxTzZXV0lFTEg0cDZMY0RSM2Z5UkkvZ2srS0JueXg0OHp4VmtBVmxscnNtZFlGdklHZDlOeTR1NkY5K2EzSEc5NjBIVUxTMS9BQ3hGTUNMM2x1bXJzZ1lVdnAxbSttTTd4cUg0UVJWZWgxNG9aUmE1aGJZMzZZUzc2bk1NTXNJME55OGFxSmpVakFEQ1hGODFGZmFia1BUajc5SkJTM0dlRU09PC9EUT48SW52ZXJzZVE+RjVoU0U5Ty9VS0pCNHlhMXMvMUdxQkZHNnpaRndmL0x5b2dPR1Jrc3pMWmQ0MUQwSFY2MVgzdEtIM2lvcWdrTE9IK0V0SFdCSXdyKy96aUlvMWVTOXVKby8yZFVPS3Z2a3VUcExDaXp6d0hkNEYrQUdHMFhJRDB1SzFDcGRhQTVQM21EZEFqV0F2dzJGZmJBTCt1WlYvRzkrUjJJYjF5RWxXTGNNRUx2L21JPTwvSW52ZXJzZVE+PEQ+clI5ZXduSlBpaVVHRjQ5dmNhaHVLc3BEVkEyc0d5QzRpZ2pKQVJPK2VkMXF2MUhJNXJya2VHMVp6Qy9MbkV0NW9FZndZQjFkNWZMMUNwOGI2a2NmNkJtWkZqV3MyNHJzQy9rNFFHNVMxcXF4Sm1MbVZRcUVIQUo3NUUvTFNLZzFzKzM0UXhMbVo1NURNMlhBRXlHYzRHVkVtdVNIejk3dDZ6L2pLMVc4bWduY3lSSGlOR0s3OVYwL2pPWFhaQ2tLMklLZ3VaRVltSXZ5NHpYQ3lZYWtsYmZLZCt3blNjZFR4aHhZeWltK0RHYVFEWlRVWUhrN1ZxUmxYMHREeVM4Mm9pTlRjajBpYis4Vm1ZRllXeXZmc0VNYWtodWlwbWVMNlJMMFNOY3lvcUwrUWJBQlRmaG43ZytacVo5VjZQUXFjMDM0LzdEdGQxYVJ4L2pMZk5Qc2dRPT08L0Q+PC9SU0FLZXlWYWx1ZT4=".decodeBase64String()
+private val QUERY_CURR_RSA_PUB_KEY =
+    "PFJTQUtleVZhbHVlPjxNb2R1bHVzPnd0L1o5OG84Z2J3OTRsYTA3QjEvQXBWQ3VIV0hHSTdQZDhGUEYzUHZOWWYxb1RZd2dSY3pRQmZQcUhmWHl0dFJSUDQ0bXFHNHRmcnoyek84Z1hFTlJTeURYdHp1N2RRR2gzaHUxdDg3VHBQYmlZY1ErWkhLNTh2NmR5MWpvMzBUVEs2NHNSbmp4SmZXcktZRHhTQnhCekRiS0NsenFsWTBKLzRtVmpLRnhrN3FTMEh2b1l5ZGxSbmh2SlZPTWRqdC9TVjZ3eUhSWTY2RnZPdmRrNkJWTG9tM0swV0JITmNGRTZDaEEzR1FjUit4eVgxWjA1OEF2aUZyeDZLUzQ1bXFSdWpVQzV2Wlh1d2J2Z3JJQ2dFVmxmT1NjSEZuclRsRlg4eXNNNEMxYlNiOEljeTNWOFhTYjdMakNtWEJlQjdUVXBXMnZqaEtsemdaZVd3TnUxRGFFdz09PC9Nb2R1bHVzPjxFeHBvbmVudD5BUUFCPC9FeHBvbmVudD48L1JTQUtleVZhbHVlPg==".decodeBase64String()
 /* ktlint-enable max-line-length */
 
 @SorapointaInternal
@@ -159,13 +161,13 @@ object DispatchConfig : DataFilePersist<DispatchConfig.Data>(
         """
         )
         val regionListClientCustomConfig: RegionListClientCustomConfig = RegionListClientCustomConfig(
-            sdkEnvironment = 2u,
+            sdkEnvironment = 2,
             showException = false,
             loadPatch = false,
             regionConfig = "",
-            regionDispatchType = 0u,
+            regionDispatchType = 0,
             videoKey = 5578228838233776,
-            downloadMode = 0u
+            downloadMode = 0
         ),
         @Comment(
             """
@@ -175,11 +177,11 @@ object DispatchConfig : DataFilePersist<DispatchConfig.Data>(
         )
         val clientCustomConfig: ClientCustomConfig = ClientCustomConfig(
             perfReportEnable = false,
-            photographShareTopics = 753u,
-            gachaShareTopics = 372u,
-            photographSharePlatform = 13u,
-            gachaSharePlatform = 13u,
-            homeItemFilter = 20u,
+            photographShareTopics = 753,
+            gachaShareTopics = 372,
+            photographSharePlatform = 13,
+            gachaSharePlatform = 13,
+            homeItemFilter = 20,
             reportNetDelayConfig = ClientCustomConfig.ReportNetDelayConfigData(
                 openGateServer = true
             )
@@ -221,16 +223,25 @@ object DispatchConfig : DataFilePersist<DispatchConfig.Data>(
         val queryCurrentRegionHardcode: String = QUERY_CURR_HARDCODE_DEFAULT,
         val currentRegionContainsCustomClientConfig: Boolean = true,
         @Comment("Response `query_cur_region` in client 2.8 or higher version format")
-        @SerialName("v2.8CurrentRegionForwardFormat")
-        val v28: Boolean = false,
+        val v28CurrentRegionForwardFormat: Boolean = false,
+        @Comment("`query_cur_region` RSA sign verify")
+        val enableSignVerify: Boolean = true,
+        @Comment(
+            """
+            If dispatch server try to forward 2.8 or higher version, 
+            dispatch server will use this RSA public key to verify signature of data
+        """
+        )
+        @SerialName("v2.8RSAPublicKey")
+        val rsaPublicKey: String = QUERY_CURR_RSA_PUB_KEY,
         @Comment(
             """
             If dispatch server try to forward 2.8 or higher version, 
             dispatch server will use this RSA key to decrypt the forwarding result.
         """
         )
-        @SerialName("v2.8RSAKey")
-        val rsaPrivateKey: String = QUERY_CURR_RSA_KEY
+        @SerialName("v2.8RSAPrivateKey")
+        val rsaPrivateKey: String = QUERY_CURR_RSA_KEY,
     )
 
     @Serializable
