75de3863c82df9801adf5524e22066d5a8055c39
WetABQ <wetabq@gmail.com>
2022-06-21 01:05:53 +0800

    feat(utils-time): add several quick way methods

--- sorapointa-utils/sorapointa-utils-time/src/main/kotlin/org/sorapointa/utils/Time.kt
+++ sorapointa-utils/sorapointa-utils-time/src/main/kotlin/org/sorapointa/utils/Time.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package org.sorapointa.utils
 
 import kotlinx.datetime.*
@@ -6,10 +8,15 @@ import java.time.ZonedDateTime
 /**
  * Just a fast path for [Clock.System.now]
  */
-@Suppress("NOTHING_TO_INLINE")
 inline fun now() = Clock.System.now()
 
 /**
  * Convert KotlinX [Instant] to Java [ZonedDateTime]
  */
 fun Instant.toZonedUtc(): ZonedDateTime = toJavaInstant().atZone(TimeZone.UTC.toJavaZoneId())
+
+inline fun nowSeconds() = now().epochSeconds
+inline fun nowMilliseconds() = now().toEpochMilliseconds()
+
+inline fun Long.millisToInstant() = Instant.fromEpochMilliseconds(this)
+inline fun Long.secondToInstant() = Instant.fromEpochSeconds(this)
