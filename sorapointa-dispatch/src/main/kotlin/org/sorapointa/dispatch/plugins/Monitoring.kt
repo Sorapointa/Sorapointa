57e751cfaa83e824235138940f1520c1b7be8913
Colerar <233hbj@gmail.com>
2022-06-30 18:40:17 +0800

    fix(log): JLineRedirector conflict with Jansi

--- sorapointa-dispatch/src/main/kotlin/org/sorapointa/dispatch/plugins/Monitoring.kt
+++ sorapointa-dispatch/src/main/kotlin/org/sorapointa/dispatch/plugins/Monitoring.kt
@@ -1,5 +1,6 @@
 package org.sorapointa.dispatch.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
@@ -7,6 +8,15 @@ import org.slf4j.event.*
 
 internal fun Application.configureMonitoring() {
     install(CallLogging) {
+        format { call ->
+            when (val status = call.response.status() ?: "Unhandled") {
+                HttpStatusCode.Found ->
+                    "${status as HttpStatusCode}: " +
+                        "${call.request} -> ${call.response.headers[HttpHeaders.Location]}"
+                "Unhandled" -> "$status: ${call.request}"
+                else -> "${status as HttpStatusCode}: ${call.request}"
+            }
+        }
         level = Level.DEBUG
         filter { call -> call.request.path().startsWith("/") }
     }
