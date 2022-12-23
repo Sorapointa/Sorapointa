b318fd9186e99f45bc17911bc1f0fe3b297ed110
WetABQ <wetabq@gmail.com>
2022-12-23 06:49:09 -0600

    refactor(utils): rewrite `Environment` utils

--- sorapointa-utils/sorapointa-utils-core/src/main/kotlin/org/sorapointa/utils/Environment.kt
+++ sorapointa-utils/sorapointa-utils-core/src/main/kotlin/org/sorapointa/utils/Environment.kt
@@ -1,19 +1,27 @@
 package org.sorapointa.utils
 
-val osName: String by lazy {
-    System.getProperty("os.name")
-}
-
 val isWindows: Boolean by lazy {
-    osName.contains("win", true)
+    osType == OsType.WINDOWS
 }
 
 val isMac: Boolean by lazy {
-    osName.contains("dar", true)
+    osType == OsType.MAC
 }
 
 val isLinux: Boolean by lazy {
-    osName.contains("nix", true) ||
-        osName.contains("nux", true) ||
-        osName.contains("aix", true)
+    osType == OsType.LINUX
 }
+
+val osType: OsType by lazy {
+    System.getProperty("os.name")?.lowercase()?.run {
+        when {
+            contains("win") -> OsType.WINDOWS
+            listOf("nix", "nux", "aix").any { contains(it) } -> OsType.LINUX
+            contains("mac") -> OsType.MAC
+            contains("sunos") -> OsType.SOLARIS
+            else -> OsType.OTHER
+        }
+    } ?: OsType.OTHER
+}
+
+enum class OsType { WINDOWS, LINUX, MAC, SOLARIS, OTHER }
